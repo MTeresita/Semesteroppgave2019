9c4d793f8ce437dc0a0a32e7382bdd4d8ca0f733
Camilla <camillahoelgaard@dhcp-10-253-0-212.wlan.hioa.no>
2019-03-29 14:46:37 +0100

    test til

--- src/main/java/org/openjfx/models/test.java
+++ src/main/java/org/openjfx/models/test.java
@@ -3,6 +3,6 @@ package org.openjfx.models;
 public class test {
 
     public void skrivUt(){
-        System.out.print("Funker kanskje?");
+        System.out.print("Funker kanskje ikke?");
     }
 }
