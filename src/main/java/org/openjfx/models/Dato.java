c0da19633416c9e655a2fab619e47bdb85ccbf10
Maria <maria.halvorsen@hotmail.no>
2019-04-02 13:02:30 +0200

    BIllett, Dato
    - Opprettet billett, Dato klasser
    - Getter og Settere

---
+++ src/main/java/org/openjfx/models/Dato.java
@@ -0,0 +1,37 @@
+package org.openjfx.models;
+
+public class Dato {
+    private int dag, måned, år;
+
+    public Dato (int dag, int måned, int år){
+        this.dag = dag;
+        this.måned = måned;
+        this.år = år;
+    }
+
+    public int getDag() {
+        return dag;
+    }
+
+    public void setDag(int dag) {
+        this.dag = dag;
+    }
+
+    public int getMåned() {
+        return måned;
+    }
+
+    public void setMåned(int måned) {
+        this.måned = måned;
+    }
+
+    public int getÅr() {
+        return år;
+    }
+
+    public void setÅr(int år) {
+        this.år = år;
+    }
+}
+
+
